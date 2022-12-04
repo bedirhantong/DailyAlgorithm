ffb5b9a4da9fb1be82fc78a73cc8ffe7fb92539b
cengb <cengbedirhantong@gmail.com>
2022-12-04 23:23:08 +0300

    preOrder,inOrder and postOrder methods added.

--- src/com/Bedirhan/uniqueExercises/binarySearchTree/BinarySearchTree.java
+++ src/com/Bedirhan/uniqueExercises/binarySearchTree/BinarySearchTree.java
@@ -25,4 +25,40 @@ public class BinarySearchTree {
     }
 
 
+    public void inorder() {
+        inorder(root);
+    }
+    public void inorder(Node root) {
+        if(root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public void preorder() {
+        preorder(root);
+    }
+    public void preorder(Node root) {
+        if(root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public void postorder() {
+        postorder(root);
+    }
+    public void postorder(Node root) {
+        if(root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+
+
+
 }
