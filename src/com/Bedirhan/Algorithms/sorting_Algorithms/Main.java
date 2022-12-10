ef9c5c92f9600e5375202c3c6bc8d141798ca572
cengb <cengbedirhantong@gmail.com>
2022-12-10 23:21:38 +0300

    Insertion sort algorithm is implemented

---
+++ src/com/Bedirhan/Algorithms/sorting_Algorithms/Main.java
@@ -0,0 +1,28 @@
+package com.Bedirhan.Algorithms.sorting_Algorithms;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(r.nextInt(10));
+        }
+    }
+    static void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i=1; i<n; ++i)
+        {
+            int deger = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] > deger)
+            {
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = deger;
+        }
+    }
+}
