6f5e7d0ddd7cf58fc5d2c4be8e3700cba0905ee2
cengb <cengbedirhantong@gmail.com>
2022-12-10 23:18:53 +0300

    Binary Search method is tested

--- src/com/Bedirhan/searching_Algorithms/binary_search/Main.java
+++ src/com/Bedirhan/searching_Algorithms/binary_search/Main.java
@@ -1,6 +1,13 @@
 package com.Bedirhan.searching_Algorithms.binary_search;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,5,8,7,11,23,35,76,99,122,123,546,1234,12345};
+        if (binarySearch(arr,4,0, arr.length-1))
+            System.out.println("yeah");
+        else
+            System.out.println("no");
+    }
 
     public static boolean binarySearch(int[] arr,int target,int low,int high){
         if (low > high)
